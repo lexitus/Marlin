dfec5571cb2f44e6a46ebc04c4848e65fc893e17
lexitus <38081592+lexitus@users.noreply.github.com>
2020-04-11 16:40:42 +0200

    Add reference for Mendel 90 bed temp sensor

--- Marlin/src/module/thermistor/thermistors.h
+++ Marlin/src/module/thermistor/thermistors.h
@@ -109,6 +109,9 @@
 #if ANY_THERMISTOR_IS(21) // Pt100 with INA826 amp with 3.3v excitation based on "Pt100 with INA826 amp on Ultimaker v2.0 electronics"
   #include "thermistor_21.h"
 #endif
+#if ANY_THERMISTOR_IS(31) // Epcos B57861S104F40 100K 155C for bed only as in original Mendel 90
+  #include "thermistor_31.h"
+#endif
 #if ANY_THERMISTOR_IS(51) // beta25 = 4092 K, R25 = 100 kOhm, Pull-up = 1 kOhm, "EPCOS"
   #include "thermistor_51.h"
 #endif
