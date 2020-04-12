8c0931a35292d4c18536b75d3011bf50ce16d6f1
Edward Perkins <ed.perkins@gmail.com>
2020-04-12 11:16:24 +0200

    increase zjerk

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -802,7 +802,7 @@
 #if ENABLED(CLASSIC_JERK)
   #define DEFAULT_XJERK 10.0
   #define DEFAULT_YJERK 10.0
-  #define DEFAULT_ZJERK  0.0
+  #define DEFAULT_ZJERK  1
 
   //#define TRAVEL_EXTRA_XYJERK 0.0     // Additional jerk allowance for all travel moves
 
